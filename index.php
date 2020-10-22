5a41de89e580d0f7234f5c27b1351db279249999
Romanie Delporte <romaniedelporte@MacBook-Pro-van-Romanie.local>
2020-10-22 22:35:56 +0200

    wip

--- index.php
+++ index.php
@@ -5,16 +5,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/scss/main.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link
     href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
     rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Eindwerk_php</title>
 </head>
-
 <body>
-  <div class="login"></div>
+  <div class="login">
   <div class="container mt-4">
     <div class="row login">
       <div class="col-6 col-md-4">
